df61d55d0a1d243efbe777c9845f1d434455774b
tangldev <tech@tangl.dev>
2024-02-04 20:22:04 +0800

    testnet branch

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -13,7 +13,7 @@ pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 // Time as measured in blocks
 // NOTE: Currently it is not possible to change the slot duration after the chain has started.
 // Attempting to do so will brick block production.
-pub const MILLISECS_PER_BLOCK: u64 = 3000;
+pub const MILLISECS_PER_BLOCK: u64 = 1200;
 // pub const BLOCKS_PER_METRIC_MINUTE: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
 pub const MINUTE: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
 pub const HOUR: BlockNumber = MINUTE * 60;
@@ -64,7 +64,7 @@ pub const MAX_ON_CHAIN_ELECTABLE_TARGETS: u32 = 200;
 /// max N of voters
 pub const MAX_ON_CHAIN_ELECTING_VOTERS: u32 = 1000;
 /// Number of blocks constituting a session.
-pub const SESSION_PERIOD: BlockNumber = 1 * DAY;
+pub const SESSION_PERIOD: BlockNumber = 30 * MINUTE;
 /// Offset time before starting the next session.
 pub const SESSION_OFFSET: BlockNumber = 1 * MINUTE;
 /// wait time in era duration until a slash is executed
